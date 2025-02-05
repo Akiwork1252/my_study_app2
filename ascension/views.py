400dc339f0693934b8064b28f9a533472a040281
Akiwork1252 <aki1252work@gmail.com>
2025-02-05 18:57:22 +0900

    CREATE:Authentication function and inquiry form

--- ascension/views.py
+++ ascension/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+
+from .forms import InquiryForm
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'ascension/index.html'
+
+class InquiryView(FormView):
+    template_name = 'ascension/inquiry.html'
+    form_class = InquiryForm
