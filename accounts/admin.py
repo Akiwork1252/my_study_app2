52bf909e2b509ce0ea80710b6b94b91ac6d149cb
Akiwork1252 <aki1252work@gmail.com>
2025-02-07 18:25:13 +0900

    Create:create learning objective form

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 from .models import CustomUser
+from ascension.models import Category, InterestCategory, UserInterest, LearningObjective, LearningMainTopic, LearningSubTopic
+from analytics.models import Progress
+
 
 # Register your models here.
 admin.site.register(CustomUser)
+admin.site.register(Category)
+admin.site.register(InterestCategory)
+admin.site.register(UserInterest)
+admin.site.register(LearningObjective)
+admin.site.register(LearningMainTopic)
+admin.site.register(LearningSubTopic)
+admin.site.register(Progress)
--- ascension/urls.py
+++ ascension/urls.py
@@ -6,4 +6,10 @@ app_name = 'ascension'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
+    path('interest_category_list/', views.InterestCategoryListView.as_view(), name='interest_category_list'),
+    path('add_interest_category/', views.AddInterestCategoryView.as_view(), name='add_interest_category'),
+    path('delete_interest_category/<int:category_id>/', views.DeleteInterestCategoryView.as_view(), name='delete_interest_category'),
+    path('learning_object/category/<int:category_id>/', views.LearningObjectiveListView.as_view(), name='learning_objective_list'),
+    path('create_learning_ovjective/<int:category_id>/', views.CreateLearningObjectiveView.as_view(), name='create_learning_objective'),
+
 ]
